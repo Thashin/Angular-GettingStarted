90f33e647059edfedd3509bc25e09f071330818a
Thashin Naidoo <Thashin@live.com>
2018-06-12 19:43:44 +0800

    More on Components

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {ProductListComponent} from './products/product-list.component';
 import {FormsModule} from '@angular/forms';
+import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   imports: [
@@ -12,7 +13,8 @@ import {FormsModule} from '@angular/forms';
   ],
   declarations: [
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   providers: [],
   bootstrap: [AppComponent]
