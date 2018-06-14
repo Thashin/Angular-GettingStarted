87ed10ad899cd03c8110fd70ef10f7d8a79e7de1
Thashin Naidoo <Thashin@live.com>
2018-06-14 20:01:00 +0800

    Navigation and Routing Basics

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -6,18 +6,30 @@ import {ProductListComponent} from './products/product-list.component';
 import {FormsModule} from '@angular/forms';
 import {ConvertToSpacesPipe} from './shared/convert-to-spaces.pipe';
 import {StarComponent} from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import {WelcomeComponent} from './home/welcome.component';
+import {RouterModule} from '@angular/router';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: 'products', component: ProductListComponent},
+      { path: 'products/:id', component: ProductDetailComponent},
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ])
   ],
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
